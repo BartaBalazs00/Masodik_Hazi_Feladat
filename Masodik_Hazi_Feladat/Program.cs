6f577f4f0c20e8903ccf01c48a348ba84b39a31a
Barta Balázs <barta.balazs00@gmail.com>
2022-10-19 18:40:08 +0200

    Add project files.

---
+++ Masodik_Hazi_Feladat/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masodik_Hazi_Feladat
+{
+    class Program
+    {
+        static bool MegegyeznekE(char karakter1, char karakter2)
+        {
+            string megoldas = "";
+            if (karakter1 == '{' && karakter2 == '}')
+            {
+                return true;
+            }
+            else if (karakter1 == '<' && karakter2 == '>')
+            {
+                return true;
+            }
+            else if (karakter1 == '[' && karakter2 == ']')
+            {
+                return true;
+            }
+            else if (karakter1 == '(' && karakter2 == ')')
+            {
+                return true;
+            }
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+            string sor = Console.ReadLine();
+            while (sor.Length < 1 || sor.Length > 10000)
+            {
+                sor = Console.ReadLine();
+            }
+            
+            string zarojelek = "()[]{}<>";
+            string levagottSor = "";
+            string megoldas = "Valid";
+            for (int i = 0; i < sor.Length; i++)
+            {
+                int index = 0;
+                while (index < zarojelek.Length && zarojelek[index] != sor[i] )
+                {
+                    index++;
+                }
+                if (index < zarojelek.Length)
+                {
+                    levagottSor += zarojelek[index];
+                }
+            }
+            if (levagottSor.Length %2 == 1)
+            {
+                megoldas = "Invalid";
+            }
+            else
+            {
+            }
+
+            Console.WriteLine(megoldas);
+            Console.ReadLine();
+        }
+    }
+}
